772cd6ce72a231f2d5f6326c29e6070fea70eea3
Warren Brown <warren@katapultmedia.com>
2019-10-26 10:43:08 -0500

    chore: added frontend and configuration

--- app.js
+++ app.js
@@ -3,8 +3,16 @@ const connectDb = require('./config/db');
 const app = express();
 const books = require('./routes/api/books');
 
+var cors = require('cors');
+
 // Connect Database
 connectDb();
+
+// cors
+app.use(cors({ origin: true, credentials: true}));
+
+// Init Middleware
+app.use(express.json({ extended: false}));
 
 app.use('/api/books', books);
 
---
+++ client/frontend/src/App.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';import './App.css';
+import './App.css';
+
+import CreateBook from './components/CreateBook';
+import ShowBookList from './components/ShowBookList';
+import ShowBookDetails from './components/ShowBookDetails';
+import UpdateBookInfo from './components/UpdateBookInfo';
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div>
+          <Route exact path='/' component={ShowBookList} />
+          <Route exact path='/create-book' component={CreateBook} />
+          <Route exact path='/edit-bo0k/:id' component={UpdateBookInfo} />
+          <Route exact path='/show-book/:id' component={ShowBookDetails} />
+        </div>
+      </Router>
+    )
+  }
+}
+export default App;
